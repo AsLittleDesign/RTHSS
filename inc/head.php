120fad8a2d9ac45f02c41506e591078f1d4b57aa
statecollege <davesmccarthy@gmail.com>
2014-08-10 16:39:20 -0700

    Website Skeleton

---
+++ inc/head.php
@@ -0,0 +1,26 @@
+
+	<!-- Meta -->
+
+<title>Rescue Tech | Health and Safety Services</title>
+<meta charset="utf-8">
+<meta name="language" content="English"> 
+<meta name="viewport" content="width=device-width, user-scalable=no">
+
+	<!-- CSS -->
+
+<!-- Latest compiled and minified Bootstrap CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<!-- Custom CSS -->
+<link rel="stylesheet" type="text/css" href="assets/stylesheets/main.css">
+
+	<!-- JS -->
+	
+<!-- jQuery -->
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<!-- Latest compiled and minified Bootstrap JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!-- Custom JS -->
+<script type="text/javascript" src="scripts/main.js"></script>
+
+
+
