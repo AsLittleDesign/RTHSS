1b56a45007744485826622f6a9b25f533138f398
statecollege <davesmccarthy@gmail.com>
2014-08-10 17:22:08 -0700

    updates

--- inc/nav.php
+++ inc/nav.php
@@ -14,16 +14,16 @@
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
 			<ul class="nav navbar-nav navbar-right">
-				<li class="active"><a href="#">Link</a></li>
-				<li><a href="#">Link</a></li>
+				<li class="active"><a href="#">About Us</a></li>
+				<li><a href="#">Contact</a></li>
 				<li><a href="#">Link</a></li>
 				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Classes <b class="caret"></b></a>
 					<ul class="dropdown-menu">
 						<li><a href="#">Action</a></li>
 						<li><a href="#">Another action</a></li>
 						<li><a href="#">Something else here</a></li>
-						<li><a href="#">Separated link</a></li>
+						<li><a href="#">Register</a></li>
 					</ul>
 				</li>
 			</ul>
--- index.php
+++ index.php
@@ -9,6 +9,21 @@
 	<?php
 		include('inc/nav.php');
 	?>
+	<section>
+		<div class="background">
+			<div class="darken">
+				<div class="center-content">
+					<div class="container">
+						<img src="assets/img/logo-symbol.svg">
+						<h1>Rescue Tech <small>Health and Safety Services</small></h1>
+						<h3>Training You for a Safer Tomorrow.</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="section">
+	</section>
 
 </body>
 </html>
